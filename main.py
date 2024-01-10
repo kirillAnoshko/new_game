bae0805a62886112cbb8d56dad925aa5c6f42f8c
kirillAnoshko <112968612+kirillAnoshko@users.noreply.github.com>
2024-01-10 19:44:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,35 +4,48 @@ import sys
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1600, 900))
+        self.screen = pygame.display.set_mode((800, 600))
         pygame.display.set_caption("Моя игра")
         self.clock = pygame.time.Clock()
         self.is_running = True
 
-    def run(self):
-        while self.is_running:
-            self.handle_events()
-            self.update()
-
-            pygame.draw.rect(self.screen, (255, 0, 0), (300, 200, 50, 50))
-            pygame.display.flip()
-
-            # Очистка экрана
-            self.screen.fill((0, 0, 0))
-
-            # Установка частоты кадров
-            self.clock.tick(60)
-        pygame.quit()
+    def render(self):
+        self.rect = pygame.Rect(300, 200, 50, 50)
+        pygame.draw.rect(self.screen, (0, 255, 0), self.rect)
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.is_running = False
 
-    def update(self):  # Обновление экрана
-        pygame.display.flip()
+    def move(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.rect.y -= 15
+                if event.key == pygame.K_DOWN:
+                    self.rect.y += 15
+                if event.key == pygame.K_LEFT:
+                    self.rect.x -= 15
+                if event.key == pygame.K_RIGHT:
+                    self.rect.x += 15    
 
+    def update(self):
+       pygame.display.update()
+    
+    def run(self):
+        while self.is_running:
+            self.handle_events()
+            self.render()
+            self.move()
+            self.update()
+            
+            self.screen.fill((0, 0, 0))
 
+            # Установка частоты кадров
+            self.clock.tick(60)
+        pygame.quit()    
+    
 # Создание экземпляра игры и запуск игрового цикла
 game = Game()
 game.run()
